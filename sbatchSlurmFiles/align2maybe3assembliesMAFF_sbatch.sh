4d1a83ebe787bad7d1141053dd78fd5de1868caa
Cristian Stark <cristian.stark@bayer.com>
2023-10-03 14:58:58 -0700

    Update align2maybe3assembliesMAFF_sbatch.sh

--- sbatchSlurmFiles/align2maybe3assembliesMAFF_sbatch.sh
+++ sbatchSlurmFiles/align2maybe3assembliesMAFF_sbatch.sh
@@ -4,7 +4,7 @@
 #SBATCH -o /home/vale878/phgSbatchLogs/out-%A_PHGanchorwave.txt
 #SBATCH -e /home/vale878/phgSbatchLogs/error-%A_PHGanchorwave.txt
 #SBATCH -t 24:00:00
-#SBATCH --partition=bigmemm
+#SBATCH --partition=high2
 
 module load apptainer
 apptainer run phg40.simg /tassel-5-standalone/run_pipeline.pl -configParameters phg/maff_from_anchorwave_config.txt -AssemblyMAFFromAnchorWavePlugin -endPlugin
