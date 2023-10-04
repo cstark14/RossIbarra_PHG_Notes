83e7238513b7382017c1d467bd24388ef6d8a90f
Cristian Stark <cristian.stark@bayer.com>
2023-10-04 10:50:11 -0700

    Update minimapDebug.sh

--- sbatchSlurmFiles/minimapDebug.sh
+++ sbatchSlurmFiles/minimapDebug.sh
@@ -4,7 +4,7 @@
 #SBATCH -o /home/vale878/out-%A_minimap2.txt
 #SBATCH -e /home/vale878/rror-%A_minimap2.txt
 #SBATCH -t 04:00
-#SBATCH --partition=bigmemm
+#SBATCH --partition=high2
 
 module load minimap2
 minimap2 -x splice -t 2 -k 12 -a -p 0.4 -N20 phg/inputDir/reference/Zm-B73-REFERENCE-NAM-5.0.fa phg/inputDir/assemblies/maff/refCDS.fa
