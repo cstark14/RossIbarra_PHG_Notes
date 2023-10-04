775555434fad25274d172cad04898be2fbfc74bd
Cristian Stark <cristian.stark@bayer.com>
2023-10-04 10:36:18 -0700

    Create minimapDebug.sh

---
+++ sbatchSlurmFiles/minimapDebug.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -l
+#SBATCH -D /home/vale878/
+#SBATCH -J debuggingMinimapKill
+#SBATCH -o /home/vale878/out-%A_minimap2.txt
+#SBATCH -e /home/vale878/rror-%A_minimap2.txt
+#SBATCH -t 24:00:00
+#SBATCH --partition=high2
+
+module load minimap2
+minimap2 -x splice -t 2 -k 12 -a -p 0.4 -N20 phg/inputDir/reference/Zm-B73-REFERENCE-NAM-5.0.fa phg/inputDir/assemblies/maff/refCDS.fa
