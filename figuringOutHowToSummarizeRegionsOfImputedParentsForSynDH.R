67bb0070be6faf01a23ecc809b92586298ad7813
Cristian Stark <crstark@ucdavis.edu>
2024-10-22 14:26:01 -0700

    added code to combine contiguous and plot
    also added some example plots and from prev commit data

--- figuringOutHowToSummarizeRegionsOfImputedParentsForSynDH.R
+++ figuringOutHowToSummarizeRegionsOfImputedParentsForSynDH.R
@@ -1,32 +1,49 @@
 library(tidyverse)
 library(dplyr)
 
-data <- data.frame(
-  location = c(1, 2, 3, 5, 6, 7, 10),
-  label = c("A", "A", "B", "B", "B", "A", "A")
-)
+fromChatGPT <- function(){
+  data <- data.frame(
+    location = c(1, 2, 3, 5, 6, 7, 10),
+    label = c("A", "A", "B", "B", "B", "A", "A")
+  )
+  
+  combined_data <- data %>%
+    mutate(group = cumsum(c(1, diff(location) != 1 | lag(label) != label))) %>%
+    group_by(group, label) %>%
+    summarize(start = min(location), end = max(location)) %>%
+    ungroup() %>%
+    select(-group)
+  
+  combined_data <- data %>%
+    mutate(group = cumsum(c(TRUE, diff(location) != 1 | lag(label, default = first(label)) != label))) %>%
+    group_by(group, label) %>%
+    summarize(start = min(location), end = max(location), .groups = 'drop') %>%
+    select(-group)
+  
+  combined_data <- data %>%
+    mutate(new_group = (location != lag(location, default = first(location)) + 1) | (label != lag(label, default = first(label)))) %>%
+    mutate(group = cumsum(new_group)) %>%
+    group_by(group, label) %>%
+    summarize(start = min(location), end = max(location), .groups = 'drop') %>%
+    select(-group)##, -new_group)
+}
 
-combined_data <- data %>%
-  mutate(group = cumsum(c(1, diff(location) != 1 | lag(label) != label))) %>%
-  group_by(group, label) %>%
-  summarize(start = min(location), end = max(location)) %>%
-  ungroup() %>%
-  select(-group)
-
-combined_data <- data %>%
-  mutate(group = cumsum(c(TRUE, diff(location) != 1 | lag(label, default = first(label)) != label))) %>%
-  group_by(group, label) %>%
-  summarize(start = min(location), end = max(location), .groups = 'drop') %>%
-  select(-group)
-
-combined_data <- data %>%
-  mutate(new_group = (location != lag(location, default = first(location)) + 1) | (label != lag(label, default = first(label)))) %>%
+currentSampleMappingImputedParents <- read_delim("testMicahPHGwithsynDH/examplesFromImputeParents/ZeaSynDH_Trial1100:250459604_HHLVGBGXX_1_GCGGATGT_readMapping.txt_imputed_parents.txt",
+                                                 delim = "\t", escape_double = FALSE, col_types = cols(sample2 = col_skip()), trim_ws = TRUE)
+combinedRegionParents <- currentSampleMappingImputedParents %>%
+  filter(!startsWith(chrom,"scaf")) %>%
+  mutate(new_group = (sample1 != lag(sample1, default = first(sample1))) | (chrom != lag(chrom, default = first(chrom)))) %>%
   mutate(group = cumsum(new_group)) %>%
-  group_by(group, label) %>%
-  summarize(start = min(location), end = max(location), .groups = 'drop') %>%
-  select(-group, -new_group)
+  group_by(group, sample1,chrom) %>%
+  summarize(start = min(start), end = max(end), .groups = 'drop') %>%
+  select(-group)
 
-ZeaSynDH_Trial10_250458543_H5NWTBGXX_1_TTCGGAC_readMapping_txt_imputed_parents <- read_delim("testMicahPHGwithsynDH/examplesFromImputeParents/ZeaSynDH_Trial10:250458543_H5NWTBGXX_1_TTCGGAC_readMapping.txt_imputed_parents.txt", 
-                                                                                             +     delim = "\t", escape_double = FALSE, 
-                                                                                             +     col_types = cols(sample2 = col_skip()), 
-                                                                                             +     trim_ws = TRUE)
+#perChrPlot <- ggplot(combinedRegionParents,aes(x=start,y=sample1,color=sample1)) + geom_point() + facet_wrap(~chrom)
+#perChrPlot
+
+#perChrPlotSegments <- ggplot(combinedRegionParents) + geom_segment(aes(x=start,xend=end,y=sample1,yend=sample1,colour=sample1)) + facet_wrap(~chrom)
+#perChrPlotSegments
+
+combinedRegionParentsChr1 <- combinedRegionParents %>% filter(chrom=="chr1")
+perChr1PlotSegments <- ggplot(combinedRegionParentsChr1) + geom_segment(aes(x=start,xend=end,y=sample1,yend=sample1,color=sample1),linewidth=5)
+perChr1PlotSegments
