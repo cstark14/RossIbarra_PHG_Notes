6ff4e417feac5aabc0f8c099fbf3342c14f786ad
Cristian Stark <crstark@ucdavis.edu>
2024-10-23 14:15:25 -0700

    adding sbatch for 0.9 prob path finding

---
+++ testMicahPHGwithsynDH/phgFindPaths_SynDHgbs_2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+#SBATCH -D /group/jrigrp11/cstark/filesFromMicahK_Danforth/
+#SBATCH -J phgReadMap_SynDHgbs_2
+#SBATCH -o /group/jrigrp11/cstark/filesFromMicahK_Danforth/out-%A_gzip.txt
+#SBATCH -e /group/jrigrp11/cstark/filesFromMicahK_Danforth/error-%A_gzip.txt
+#SBATCH -t 24:00:00
+#SBATCH --partition=high2
+#SBATCH --mem=122880
+
+module load conda
+module load jdk
+
+export PATH="/group/jrigrp11/cstark/phg/bin:$PATH"
+export JAVA_OPTS="-Xmx100g"
+
+phg map-kmers \
+    --hvcf-dir /group/jrigrp11/cstark/filesFromMicahK_Danforth/phg_v2.4.8.162_ZeaSyn/output/vcf_files \
+    --key-file /group/jrigrp11/cstark/synDHfastqPHGmapped/fastqList2_phgMappingKeyFile.txt \
+    --output-dir /group/jrigrp11/cstark/synDHfastqPHGmapped/ > /group/jrigrp11/cstark/mappingfastqList2_20241021.out 2> /group/jrigrp11/cstark/mappingfastqList2_20241021.err
---
+++ testMicahPHGwithsynDH/phgFindPaths_SynDHgbs_3.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+#SBATCH -D /group/jrigrp11/cstark/filesFromMicahK_Danforth/
+#SBATCH -J phgReadMap_SynDHgbs_3
+#SBATCH -o /group/jrigrp11/cstark/filesFromMicahK_Danforth/out-%A.txt
+#SBATCH -e /group/jrigrp11/cstark/filesFromMicahK_Danforth/error-%A.txt
+#SBATCH -t 24:00:00
+#SBATCH --partition=high2
+#SBATCH --mem=122880
+
+module load conda
+module load jdk
+
+export PATH="/group/jrigrp11/cstark/phg/bin:$PATH"
+export JAVA_OPTS="-Xmx100g"
+
+phg find-paths \
+    --path-keyfile /group/jrigrp11/cstark/readMappedSynDHgbs_keyfile.txt \
+    --hvcf-dir /group/jrigrp11/cstark/filesFromMicahK_Danforth/phg_v2.4.8.162_ZeaSyn/output/vcf_files \
+    --reference-genome /group/jrigrp11/cstark/filesFromMicahK_Danforth/phg_v2.4.8.162_ZeaSyn/output/updated_assemblies/B73.fa \
+    --path-type haploid \
+    --output-dir /group/jrigrp11/cstark/synDH_vcfFilesImputedProbLikely0.9 \
+    --prob-correct=0.90 \
+    --out-parents-dir /group/jrigrp11/cstark/synDH_likelyAncestorsProbLikely0.9 > /group/jrigrp11/cstark/synDH_likelyAncestorsProbLikely0.9/pathFinding3_prob0.90_20241023.out 2> /group/jrigrp11/cstark/synDH_likelyAncestorsProbLikely0.9/pathFinding3_prob0.90_20241023.err
