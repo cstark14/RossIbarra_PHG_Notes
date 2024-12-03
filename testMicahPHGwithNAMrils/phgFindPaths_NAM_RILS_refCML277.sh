036212709e3afb75e15cde481a8d5db88f2ac58c
Cristian Stark <crstark@ucdavis.edu>
2024-12-03 12:11:10 -0800

    Create phgFindPaths_NAM_RILS_refCML277.sh

---
+++ testMicahPHGwithNAMrils/phgFindPaths_NAM_RILS_refCML277.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -l
+#SBATCH -D /group/jrigrp11/cstark/
+#SBATCH -J phgNamRILSimpute0.90_CML277_20241203
+#SBATCH -o /group/jrigrp11/cstark/namRIL_GBS/%J_%j.sbatch.out
+#SBATCH -e /group/jrigrp11/cstark/namRIL_GBS/%J_%j.sbatch.err
+#SBATCH -t 24:00:00
+#SBATCH --partition=high2
+#SBATCH --ntasks=30
+#SBATCH --mem=240000
+
+module load conda
+module load jdk
+
+export PATH="/group/jrigrp11/cstark/phg/bin:$PATH"
+export JAVA_OPTS="-Xmx240g"
+
+phg find-paths \
+    --path-keyfile /group/jrigrp11/cstark/RossIbarra_PHG_Notes/testMicahPHGwithNAMrils/NAM_RILs_SRP009896_imputeKeyfile.txt \
+    --hvcf-dir /group/jrigrp11/cstark/filesFromMicahK_Danforth/phg_v2.4.8.162_ZeaSyn/output/vcf_files \
+    --reference-genome /group/jrigrp11/cstark/filesFromMicahK_Danforth/phg_v2.4.8.162_ZeaSyn/output/updated_assemblies/CML277.fa.gz \
+    --path-type haploid \
+    --output-dir /group/jrigrp11/cstark/namRIL_GBS/imputed_prob0.90_CML277 \
+    --prob-correct=0.90 \
+    --out-parents-dir /group/jrigrp11/cstark/namRIL_GBS/imputed_prob0.90_CML277/parents > /group/jrigrp11/cstark/namRIL_GBS/phgNamRILSimpute_Acc0.90_CML277_20241203.out 2> /group/jrigrp11/cstark/namRIL_GBS/phgNamRILSimpute_Acc0.90_CML277_20241203.err
