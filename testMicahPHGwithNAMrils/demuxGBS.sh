760dc329c153b83e7419b627f0948d9013352151
Cristian Stark <crstark@ucdavis.edu>
2024-12-04 17:03:14 -0800

    Create demuxGBS.sh

---
+++ testMicahPHGwithNAMrils/demuxGBS.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+#SBATCH -D /group/jrigrp11/cstark/
+#SBATCH -J demuxGBS
+#SBATCH -o /group/jrigrp11/cstark/namRIL_GBS/logs/%x_%j.sbatch.out
+#SBATCH -e /group/jrigrp11/cstark/namRIL_GBS/logs/%x_%j.sbatch.err
+#SBATCH -t 24:00:00
+#SBATCH --partition=high2
+#SBATCH --ntasks=30
+#SBATCH --mem=240000
+
+pip install demultiplex
+
+demultiplex demux -r --format unknown ../../RossIbarra_PHG_Notes/testMicahPHGwithNAMrils/sampleNamesBarcodes.txt ../rawReads/*
+demultiplex demux -r -m5 -d -p /group/jrigrp11/cstark/namRILS_GBS/rawReads_demuxByName/. ../../RossIbarra_PHG_Notes/testMicahPHGwithNAMrils/sampleNamesBarcodes.txt \
+    /group/jrigrp11/cstark/namRILS_GBS/rawReads/SRR391097.fastq.gz 
